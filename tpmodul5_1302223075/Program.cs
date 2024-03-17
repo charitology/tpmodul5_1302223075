d8c94001ca0d2ade1bf54af355f9c347e0045420
Dara Sheiba <102475225+charitology@users.noreply.github.com>
2024-03-17 22:20:40 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223075/Program.cs
+++ tpmodul5_1302223075/Program.cs
@@ -1,15 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 public class HaloGeneric
 {
-    public void SapaPengguna (string nama)
+    public void SapaPengguna<X>(X nama)
     {
         Console.WriteLine("Halo Pengguna " + nama + "!");
 
     }
+    public class DataGeneric<T>
+    {
+        public T data { get; set; }
+
+        public DataGeneric(T data)
+        {
+        this.data = data;
+        }
+
+        public void PrintData()
+        {
+        Console.WriteLine("Data yang tersimpan: " + data);
+        }
+    }
+    
     public static void Main(string[] args)
     {
         HaloGeneric halo = new HaloGeneric();
+        Console.WriteLine("Dengan siapa disini?");
         string nama = Console.ReadLine();
         halo.SapaPengguna(nama);
+
+        Console.WriteLine("Berapa NIM-mu?");
+        string nim = Console.ReadLine();
+        DataGeneric<string> dataStr = new DataGeneric<string>(nim);
+        dataStr.PrintData();
+
+    }
+}
+
+internal class DataGeneric<T>
+{
+    private string? nim;
+
+    public DataGeneric(string? nim)
+    {
+        this.nim = nim;
     }
 }
